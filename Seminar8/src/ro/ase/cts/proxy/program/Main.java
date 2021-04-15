ca8078f66dfc5ee01f03d90be1a15ffdadb8a409
Tania <Tania@DESKTOP-DF4TVQB>
2021-04-15 12:36:03 +0300

    Commit 2

---
+++ Seminar8/src/ro/ase/cts/proxy/program/Main.java
@@ -0,0 +1,18 @@
+package ro.ase.cts.proxy.program;
+
+import ro.ase.cts.proxy.clase.OperatorRezervare;
+import ro.ase.cts.proxy.clase.Proxy;
+
+public class Main {
+
+	public static void main(String[] args) {
+		OperatorRezervare operatorRezervare = new OperatorRezervare("Local X");
+		operatorRezervare.rezerva(12);
+		operatorRezervare.rezerva(3);
+		
+		Proxy proxy = new Proxy(operatorRezervare,4); 
+		proxy.rezerva(2);
+		proxy.rezerva(7);
+	}
+
+}
